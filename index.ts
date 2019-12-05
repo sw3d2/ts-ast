a3c05c2976d06b162fab1e8e1370d3ee17d24da2
sw3d <>
2019-12-04 22:48:01 -0800

    Print interfaces too

--- index.ts
+++ index.ts
@@ -4,22 +4,28 @@ function compile(fileNames: string[], options: ts.CompilerOptions): void {
   let program = ts.createProgram(fileNames, options);
 
   for (let file of program.getSourceFiles()) {
-    if (/node_modules/.test(file.fileName))
-      continue;
-    console.log('src file:', file.fileName);
+    /* if (/node_modules/.test(file.fileName))
+      continue; */
+    console.log(file.fileName);
     ts.forEachChild(file, node => {
-      console.log('ast node:', getNodeSummary(node, file));
+      if (node.kind == ts.SyntaxKind.EndOfFileToken)
+        return;
+      console.log(getNodeSummary(node, file));
     });
   }
 }
 
 function getNodeSummary(node: ts.Node, file: ts.SourceFile) {
   if (ts.isFunctionDeclaration(node))
-    return node.name!.text;
+    return node.name!.text + '()';
   if (ts.isVariableStatement(node))
-    return node.declarationList.declarations[0].name.getText(file);
+    return node.declarationList.declarations[0].name.getText(file) + '=?';
   if (ts.isInterfaceDeclaration(node))
-    return node.name.text;
+    return node.name.text + '{}';
+  if (ts.isTypeAliasDeclaration(node))
+    return 'type ' + node.name.text + ' = ?';
+  if (ts.isModuleDeclaration(node))
+    return 'module ' + node.name.text + ' {}';
   return node.kind;
 }
 
